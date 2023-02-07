ec59dac96cec94962a23b5085824c685eb5d5251
Matthew Carney <matthewcarney64@gmail.com>
2023-02-07 21:31:50 +0000

    Some basic checks

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ using SC4Parser.Subfiles;
 using SC4Parser.Types;
 using SC4Parser.Logging;
 
-namespace SC4Extractor // Note: actual namespace depends on the project name.
+namespace SC4Diff
 {
     internal class Program
     {
@@ -25,20 +25,21 @@ namespace SC4Extractor // Note: actual namespace depends on the project name.
                     "For example: SC4Diff C:\\Path\\To\\First\\Simcity\\Save.sc4 C:\\Path\\To\\Second\\Simcity\\Save.sc4");
                 return;
             }
+
             
             // Check files exist
-            if (File.Exists(args[0]))
+            if (!File.Exists(args[0]))
             {
                 Console.WriteLine("Could not file file: " + args[0]);
                 return;
             }
-            if (File.Exists(args[1]))
+            if (!File.Exists(args[1]))
             {
                 Console.WriteLine("Could not file file: " + args[1]);
                 return;
             }
             
-            // Check extensions
+            // TODO: Check extensions
             try
             {
                 using (SC4SaveFile saveGame1 = new(args[0]))
@@ -50,7 +51,56 @@ namespace SC4Extractor // Note: actual namespace depends on the project name.
                     entries1.OrderBy(x => x.TGI);
                     entries2.OrderBy(x => x.TGI);
 
+                    List<TypeGroupInstance> missingEntries = new List<TypeGroupInstance>();
+                    List<TypeGroupInstance> matchingEntries = new List<TypeGroupInstance>();
 
+                    for (int i = 0; i < entries1.Count; i++)
+                    {
+                        IndexEntry sourceEntry = entries1[i];
+                        
+                        IndexEntry foundEntry = null;
+                        for (int j = 0; j < entries2.Count; j++)
+                        {
+                            if (sourceEntry.TGI == entries2[j].TGI)
+                            {
+                                foundEntry = entries2[j];
+                                break;
+                            }
+                        }
+
+                        if (foundEntry == null)
+                        {
+                            missingEntries.Add(sourceEntry.TGI);
+
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("{0}: Entry missing",
+                                sourceEntry.TGI);
+                        }
+                        else
+                        {
+                            matchingEntries.Add(sourceEntry.TGI);
+
+                            // Compare size of two entries
+                            if (sourceEntry.FileSize != foundEntry.FileSize)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                                Console.WriteLine("[{0}] Entry has a different filesize [{1} -> {2}]",
+                                    sourceEntry.TGI.ToString(),
+                                    sourceEntry.FileSize,
+                                    foundEntry.FileSize);
+                                Console.ResetColor();
+                            }
+                            if (sourceEntry.FileLocation != foundEntry.FileLocation)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                                Console.WriteLine("[{0}] Entry is in a different location [0x{1} -> 0x{2}]",
+                                    sourceEntry.TGI.ToString(),
+                                    sourceEntry.FileSize.ToString("X8"),
+                                    foundEntry.FileSize.ToString("X8"));
+                                Console.ResetColor();
+                            }
+                        }
+                    }
                 }
             }
             catch (Exception ex)
